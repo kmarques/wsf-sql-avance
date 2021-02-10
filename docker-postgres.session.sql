5f4e26774257b3f71720d47947bded31449f3e83
Karl MARQUES <kmarques@vetixy.com>
2021-02-10 10:45:17 +0100

    Third commit

--- docker-postgres.session.sql
+++ docker-postgres.session.sql
@@ -129,4 +129,57 @@ SELECT animal.id,
     lastname,
     'edit' as mode
 FROM animal
-    INNER JOIN user_account ON animal.owner = user_account.id
+    INNER JOIN user_account ON animal.owner = user_account.id;
+--
+SELECT animal.id,
+    ua.id,
+    ua.firstname,
+    ua.lastname,
+    ua2.id,
+    ua2.firstname,
+    ua2.lastname
+FROM animal
+    INNER JOIN user_account ua ON animal.owner = ua.id
+    INNER JOIN community.follower ON animal.id = animal_id
+    INNER JOIN user_account ua2 ON user_account_id = ua2.id;
+--
+CREATE TABLE animal_status (
+    id SERIAL PRIMARY KEY,
+    animal_id INT REFERENCES animal(id),
+    status CHAR(5) NOT NULL CHECK (status IN ('BIRTH', 'SICK', 'DEATH')),
+    date TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+INSERT INTO animal_status(animal_id, status, date)
+SELECT id,
+    'BIRTH',
+    NOW() - INTERVAL '2 years'
+FROM animal
+UNION
+SELECT 4,
+    'SICK',
+    NOW() - INTERVAL '1 year'
+UNION
+SELECT 5,
+    'SICK',
+    NOW() - INTERVAL '2 day'
+UNION
+SELECT 5,
+    'DEATH',
+    NOW() - INTERVAL '1 day';
+--
+SELECT animal_id,
+    MAX(date)
+FROM animal_status
+GROUP BY animal_id;
+--
+WITH animal_max_status AS (
+    SELECT animal_id,
+        MAX(date) AS max_date
+    FROM animal_status
+    GROUP BY animal_id
+)
+SELECT *
+FROM animal_max_status ams
+    INNER JOIN animal a ON a.id = ams.animal_id
+    INNER JOIN animal_status ast ON ams.animal_id = ast.animal_id
+    AND ams.max_date = ast.date;
